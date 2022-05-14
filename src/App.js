91bb40d4c6fe01650aaecf4f1c13e8ac0ae5199d
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2022-05-15 08:47:23 +0900

    ユーザーの操作をコンポーネントツリーの下から上に伝える

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import colorData from "./data/color-data.json";
 import ColorList from "./components/ColorList";
 
 function App() {
-  const [colors] = useState(colorData);
-  return <ColorList colors={colors} />;
+  const [colors, setColors] = useState(colorData);
+  return (
+    <ColorList
+      colors={colors}
+      onRemoveColor={(id) => {
+        const newColors = colors.filter((color) => color.id !== id);
+        setColors(newColors);
+      }}
+      onRateColor={(id, rating) => {
+        const newColors = colors.map((color) =>
+          color.id === id ? { ...color, rating } : color
+        );
+        setColors(newColors);
+      }}
+    />
+  );
 }
 
 export default App;
--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Color from "./Color";
 
-export default function ColorList({ colors = [] }) {
+export default function ColorList({
+  colors = [],
+  onRemoveColor = (f) => f,
+  onRateColor = (f) => f,
+}) {
   if (!colors.length) return <div>No Colors Listed.</div>;
   return (
     <div style={{ padding: 50 }}>
       {colors.map((color) => (
-        <Color key={color.id} {...color} />
+        <Color
+          key={color.id}
+          {...color}
+          onRemove={onRemoveColor}
+          onRate={onRateColor}
+        />
       ))}
     </div>
   );
