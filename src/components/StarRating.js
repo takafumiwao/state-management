48d692545a8c4343c165100c13501edbd3751d65
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2022-05-15 07:58:27 +0900

    初期設定、Starの作成

---
+++ src/components/StarRating.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const Star = ({ selected = false, onSelect = (f) => f }) => (
+  <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
+);
+
+export default function StarRating({ style = {}, totalStars = 5, ...props }) {
+  const [selectedStats, setSelectedStars] = useState(0);
+  return (
+    <div style={{ padding: "5px", ...style }} {...props}>
+      {[...Array(totalStars)].map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStats > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+
+      <p>
+        {selectedStats} fo {totalStars} stars
+      </p>
+    </div>
+  );
+}
