1c92f8e9eed9034c139c4d288ba66e86be114454
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2022-05-15 15:45:38 +0900

    コンテキスト経由でデータを公開する

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { colors } from "./data/color-data.json";
 
+export const ColorContext = createContext();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ColorContext.Provider value={{ colors }}>
+      <App />
+    </ColorContext.Provider>
   </React.StrictMode>
 );
